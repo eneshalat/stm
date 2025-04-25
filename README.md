53dddab4f7fb61029801cfe1236a1e01e62772b6
Enes Halat <yehalat@outlook.com>
2025-04-25 23:13:48 +0300

    Initial commit with few 'lib' code

---
+++ README.md
@@ -0,0 +1,5 @@
+# stm
+
+## A package manager. The difference is I cooked it.
+
+Copyright (C) 2025 Enes Halat
---
+++ stm/src/main.rs
@@ -0,0 +1,4 @@
+fn main() {
+    println!("Hello, STM!");
+    println!("This is Steria Package Manager.");
+}
